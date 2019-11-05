34146ba8e05a8ab17b9388e53ad4d1c3b9c13c57
kmartinrs <46468774+kmartinrs@users.noreply.github.com>
2019-11-05 16:43:24 -0200

    config to run with junit

--- web-automation-tests/src/main/java/features/Runner.java
+++ web-automation-tests/src/main/java/features/Runner.java
@@ -1,10 +1,20 @@
 package features;
 
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
+import cucumber.TestContext;
+import framework.Report;
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(glue = "stepdefinition")
 public class Runner {
-
+	@AfterClass
+	public static void teardown() {
+		if (TestContext.getDriver() != null)
+			TestContext.getDriver().quit();
+		Report.close();
+	}
 }
